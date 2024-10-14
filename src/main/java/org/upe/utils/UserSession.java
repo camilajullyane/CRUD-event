4240784a51d92cfc3dc8549481540f4dfe13e903
camilajullyane <camilaj.silva0@gmail.com>
2024-10-14 12:05:35 -0300

    fix: repetição de código e passagem do userData entre as telas

--- src/main/java/org/upe/utils/UserSession.java
+++ src/main/java/org/upe/utils/UserSession.java
@@ -1,5 +1,29 @@
 package org.upe.utils;
 
-public class UserSession {
+import org.upe.persistence.interfaces.UserInterface;
 
+public class UserSession {
+    private static UserSession instance;
+    private UserInterface currentUser;
+
+    private UserSession() {}
+
+    public static UserSession getInstance() {
+        if (instance == null) {
+            instance = new UserSession();
+        }
+        return instance;
+    }
+
+    public void setCurrentUser(UserInterface user) {
+        currentUser = user;
+    }
+
+    public UserInterface getCurrentUser() {
+        return currentUser;
+    }
+
+    public void clearSession() {
+        currentUser = null;
+    }
 }
