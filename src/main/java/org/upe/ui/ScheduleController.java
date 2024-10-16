1fc5254b5d722bf7cd34ad20f51bd15ef1673b84
camilajullyane <camilaj.silva0@gmail.com>
2024-10-16 20:57:33 -0300

    melhorando interface de login

--- src/main/java/org/upe/ui/ScheduleController.java
+++ src/main/java/org/upe/ui/ScheduleController.java
@@ -1,5 +1,11 @@
 package org.upe.ui;
 
+import javafx.fxml.FXML;
+import org.upe.controllers.EventController;
+
 public class ScheduleController {
+
+    EventController eventController = new EventController();
+
 
 }
