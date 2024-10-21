f8e2ae2e148bf45a085356a4f78ea75123e7137b
camil <camilaj.silva0@gmail.com>
2024-10-21 00:16:37 -0300

    melhorando telaInicio

--- src/main/java/org/upe/ui/BeginController.java
+++ src/main/java/org/upe/ui/BeginController.java
@@ -3,15 +3,21 @@ package org.upe.ui;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import org.upe.controllers.EventController;
+import org.upe.persistence.interfaces.EventInterface;
 import org.upe.utils.SceneLoader;
 import javafx.scene.text.Text;
 import org.upe.utils.UserSession;
 
 import java.io.IOException;
+import java.util.List;
 
 public class BeginController {
     @FXML
     Button subscriptionButton;
+
+    @FXML
+    Text infoText;
 
     @FXML
     Button createEventButton;
@@ -27,8 +33,17 @@ public class BeginController {
 
     @FXML
     private void initialize(){
+        EventController eventController = new EventController();
+        List<EventInterface> events = eventController.getAllEvents();
+
         if (UserSession.getInstance().getCurrentUser() != null) {
             userName.setText(UserSession.getInstance().getCurrentUser().getName());
+        }
+
+        if(events.isEmpty()){
+            infoText.setText("Você ainda não faz parte de nenhum evento");
+        } else {
+            infoText.setText("Você faz parte de " + events.size() + " eventos");
         }
     }
 
