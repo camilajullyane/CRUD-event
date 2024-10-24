60ef55d9addc4c616a8f68d3f619b90c51a09f57
camil <camilaj.silva0@gmail.com>
2024-10-24 19:28:21 -0300

    navegação

--- src/main/java/org/upe/ui/ScheduleController.java
+++ src/main/java/org/upe/ui/ScheduleController.java
@@ -1,6 +1,5 @@
 package org.upe.ui;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
@@ -9,8 +8,6 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
