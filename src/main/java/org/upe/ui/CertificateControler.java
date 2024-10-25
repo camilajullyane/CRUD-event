0115111541045f9c5a4186b7dcd99cf42dd0d0ed
Mergs <gabrielsnmergulhao@gmail.com>
2024-10-25 00:26:19 -0300

    mais

--- src/main/java/org/upe/ui/CertificateControler.java
+++ src/main/java/org/upe/ui/CertificateControler.java
@@ -35,45 +35,41 @@ public class CertificateControler {
 
     }
 
-    @FXML
-    private void logOut() throws IOException {
-        SceneLoader.loadScene("/org/upe/ui/start-app.fxml", "Even2", certificatePage);
-      
-    @FXML
-    private void moveToSettingsScreen() throws IOException {
-        SceneLoader.loadScene("/org/upe/ui/telaConfiguracoes.fxml", "Configurações", certificatePage);
+        @FXML
+        private void logOut() throws IOException {
+            SceneLoader.loadScene("/org/upe/ui/start-app.fxml", "Even2", certificatePage);
+        }
 
-    }
+        @FXML
+        private void moveToSettingsScreen () throws IOException {
+            SceneLoader.loadScene("/org/upe/ui/telaConfiguracoes.fxml", "Configurações", certificatePage);
 
-    @FXML
-    private void moveToScheduleScreen() throws IOException {
-        SceneLoader.loadScene("/org/upe/ui/telaProgramacao.fxml", "Programação", certificatePage);
-    }
+        }
 
-    @FXML
-    private void moveToHomeScreen() throws IOException {
-        SceneLoader.loadScene("/org/upe/ui/telaInicio.fxml", "Home", certificatePage);
-    }
+        @FXML
+        private void moveToScheduleScreen () throws IOException {
+            SceneLoader.loadScene("/org/upe/ui/telaProgramacao.fxml", "Programação", certificatePage);
+        }
 
-    @FXML
-    private void moveToSubscriptionButton() throws IOException {
-        SceneLoader.loadScene("/org/upe/ui/telaInscricoes.fxml", "Inscrições", certificatePage);
-    }
+        @FXML
+        private void moveToHomeScreen () throws IOException {
+            SceneLoader.loadScene("/org/upe/ui/telaInicio.fxml", "Home", certificatePage);
+        }
 
-    @FXML
-    private void moveToSubmissionsPage() throws IOException {
-        SceneLoader.loadScene("/org/upe/ui/telaSubmissões.fxml", "Submissões", certificatePage);
-    }
+        @FXML
+        private void moveToSubscriptionButton () throws IOException {
+            SceneLoader.loadScene("/org/upe/ui/telaInscricoes.fxml", "Inscrições", certificatePage);
+        }
 
-    @FXML
-    private void moveToOpenCertificatePage() throws IOException {
-        SceneLoader.loadScene("/org/upe/ui/telaAbrindoCertificado.fxml", "Certificado", certificatePage);
-    }
+        @FXML
+        private void moveToSubmissionsPage () throws IOException {
+            SceneLoader.loadScene("/org/upe/ui/telaSubmissões.fxml", "Submissões", certificatePage);
+        }
 
+        @FXML
+        private void moveToOpenCertificatePage () throws IOException {
+            SceneLoader.loadScene("/org/upe/ui/telaAbrindoCertificado.fxml", "Certificado", certificatePage);
+        }
 
-    @FXML
-    void initialize(ActionEvent event) {
-
-    }
 
 }
