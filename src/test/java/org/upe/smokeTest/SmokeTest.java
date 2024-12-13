7ae9dbb6533a5bd7ca903b8c11305bd2b3fa1511
CAMILA JULLYANE DOS SANTOS SILVA <12022262484@UPE.IO>
2024-12-13 10:23:45 -0300

    Adicionando smokeTest

--- src/test/java/org/upe/smokeTest/SmokeTest.java
+++ src/test/java/org/upe/smokeTest/SmokeTest.java
@@ -1,28 +1,28 @@
-// package org.upe.smokeTest;
+ package org.upe.smokeTest;
 
 
-// import javafx.stage.Stage;
-// import org.junit.jupiter.api.Test;
-// import org.testfx.framework.junit5.ApplicationTest;
-// import org.upe.ui.StartApp;
-// import java.io.IOException;
+ import javafx.stage.Stage;
+ import org.junit.jupiter.api.Test;
+ import org.testfx.framework.junit5.ApplicationTest;
+ import org.upe.ui.StartApp;
+ import java.io.IOException;
 
-// import static org.assertj.core.api.Assertions.assertThat;
-// import static org.junit.jupiter.api.Assertions.assertNotNull;
+ import static org.assertj.core.api.Assertions.assertThat;
+ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
-// public class SmokeTest extends ApplicationTest  {
-//     private Stage stage;
+ public class SmokeTest extends ApplicationTest  {
+     private Stage stage;
 
-//     @Override
-//     public void start(Stage stage) throws IOException {
-//         this.stage = stage;
-//         new StartApp().start(stage);
-//     }
+     @Override
+     public void start(Stage stage) throws IOException {
+         this.stage = stage;
+         new StartApp().start(stage);
+     }
 
-//     @Test
-//     public void testAppStart() {
-//         assertNotNull(stage, "A aplicação não foi iniciada corretamente.");
-//         assertThat(stage.getTitle()).isEqualTo("Even2");
-//     }
-// }
+     @Test
+     public void testAppStart() {
+         assertNotNull(stage, "A aplicação não foi iniciada corretamente.");
+         assertThat(stage.getTitle()).isEqualTo("Even2");
+     }
+ }
