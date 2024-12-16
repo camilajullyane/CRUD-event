7a97063c04c5651913f8c5c13079bf98a05d52f0
Camila Jullyane <camilaj.silva0@gmail.com>
2024-12-16 20:33:36 -0300

    criando teste submitArticle com mockito

--- src/test/java/org/upe/controllers/ArticleControllerTest.java
+++ src/test/java/org/upe/controllers/ArticleControllerTest.java
@@ -1,11 +1,12 @@
 package org.upe.controllers;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.upe.persistence.DAO.ArticleDAO;
 import org.upe.persistence.DAO.EventDAO;
 import org.upe.persistence.DAO.UserDAO;
@@ -14,15 +15,14 @@ import org.upe.persistence.model.Article;
 import org.upe.persistence.model.Event;
 import org.upe.persistence.model.User;
 
-import java.time.LocalDate;
 import java.util.Collections;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 public class ArticleControllerTest {
     @InjectMocks
     private ArticleController articleController;
@@ -34,30 +34,12 @@ public class ArticleControllerTest {
     @Mock
     UserDAO userDAO;
 
+    @Mock
     private static Article mockArticle;
+    @Mock
     private static User mockUser;
+    @Mock
     private static Event mockEvent;
-
-    @BeforeAll
-    static void setUp() {
-        mockArticle = Article.Builder()
-                .withTitle("Article")
-                .withArticleAbstract("Abstract")
-                .build();
-
-        mockUser = User.Builder()
-                .withName("Pam Bessly")
-                .withEmail("bessly@example.com")
-                .build();
-
-        mockEvent = Event.Builder().withName("Event").withDescription("Description")
-                .withBeginDate(LocalDate.now())
-                .withEndDate(LocalDate.now())
-                .withLocal("Local")
-                .withOrganization("Org")
-                .withOwner(mockUser)
-                .build();
-    }
 
     @Test
     void testCreateArticle() {
@@ -66,27 +48,33 @@ public class ArticleControllerTest {
         assertNotNull(result);
     }
 
-//    @Test
-//    void testSubmitArticle() {
-//        when(articleDAO.getAll()).thenReturn(Collections.singletonList(mockArticle));
-//        when(articleDAO.findById(mockArticle.getId())).thenReturn(mockArticle);
-//
-//
-//        boolean result = articleController.submitArticle(mockArticle, mockEvent);
-//
-//        assertTrue(result);
-//        verify(mockArticle).addSubmittedIn(mockEvent);
-////        verify(articleDAO).update(mockArticle);
-//        verify(mockEvent).addArticleOnEvent(mockArticle);
-////        verify(eventDAO).update(mockEvent);
-//    }
-//
-//    @Test
-//    void testDeleteArticle() {
-//        when(mockArticle.getId()).thenReturn(UUID.randomUUID());
-//        boolean result = articleController.deleteArticle(mockArticle);
-//        assertTrue(result);
-//        verify(articleDAO).delete(mockArticle.getId());
-//    }
+    @Test
+    void testSubmitArticle() {
+        when(articleDAO.getAll()).thenReturn(Collections.singletonList(mockArticle));
+        when(articleDAO.findById(mockArticle.getId())).thenReturn(mockArticle);
+
+        doNothing().when(mockArticle).addSubmittedIn(mockEvent);
+        doNothing().when(mockEvent).addArticleOnEvent(mockArticle);
+        when(articleDAO.update(mockArticle)).thenReturn(mockArticle);
+        doNothing().when(eventDAO).update(mockEvent);
+
+
+        boolean result = articleController.submitArticle(mockArticle, mockEvent);
+        assertTrue(result);
+
+
+        verify(mockArticle).addSubmittedIn(mockEvent);
+        verify(articleDAO).update(mockArticle);
+        verify(mockEvent).addArticleOnEvent(mockArticle);
+        verify(eventDAO).update(mockEvent);
+    }
+
+    @Test
+    void testDeleteArticle() {
+        when(mockArticle.getId()).thenReturn(UUID.randomUUID());
+        boolean result = articleController.deleteArticle(mockArticle);
+        assertTrue(result);
+        verify(articleDAO).delete(mockArticle.getId());
+    }
 
 }
