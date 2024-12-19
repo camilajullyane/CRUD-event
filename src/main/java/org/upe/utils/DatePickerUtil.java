671c503b96ff39a9052f98ff66882e6b5d156e5f
Victor Salgado <joaosalgado605@gmail.com>
2024-12-19 15:30:09 -0300

    Testes do DatePicker e criando datePicker para ser utilizado em subEventos

--- src/main/java/org/upe/utils/DatePickerUtil.java
+++ src/main/java/org/upe/utils/DatePickerUtil.java
@@ -3,6 +3,8 @@ package org.upe.utils;
 import javafx.scene.control.DateCell;
 import javafx.scene.control.DatePicker;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.time.LocalDate;
 
 public class DatePickerUtil {
@@ -19,6 +21,19 @@ public class DatePickerUtil {
                 LocalDate now = LocalDate.now();
                 LocalDate oneYearLater = now.plusYears(1);
                 if (item.isBefore(now) || item.isAfter(oneYearLater)) {
+                    setDisable(true);
+                    setStyle("-fx-background-color: #D9D9D9;");
+                }
+            }
+        });
+    }
+
+    public static void restrictDatePicker(DatePicker datePicker, LocalDate eventStartDate, LocalDate eventEndDate) {
+        datePicker.setDayCellFactory(dp -> new DateCell() { // Usando lambda para Callback
+            @Override
+            public void updateItem(LocalDate item, boolean empty) {
+                super.updateItem(item, empty);
+                if (item.isBefore(eventStartDate) || item.isAfter(eventEndDate)) {
                     setDisable(true);
                     setStyle("-fx-background-color: #D9D9D9;");
                 }
